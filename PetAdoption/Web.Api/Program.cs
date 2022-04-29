4fecc6715971f67a88e552639624d651663ed742
Zhixin He <zhixin.he@hotmail.com>
2022-04-29 02:08:08 -0700

    web.DataBase and Web.Api is completed

--- PetAdoption/Web.Api/Program.cs
+++ PetAdoption/Web.Api/Program.cs
@@ -1,5 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using WebData.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<PetDbContext>(opts=> {
+    opts.UseSqlServer(builder.Configuration["ConnectionStrings:DevelopmentConnection"]);
+});
+builder.Services.AddScoped<IPetRepository, EFPetRepository>();
+builder.Services.AddScoped<IAccountRepository, EFAccountRepository>();
+builder.Services.AddScoped<IMessageRepository, EFMessageRepository>();
+builder.Services.AddScoped<IPetWaitingListRepository, EFPetWaitingListRepository>();
+
+builder.Services.AddControllers();
 var app = builder.Build();
+
+app.MapControllers();
 
 app.MapGet("/", () => "Hello World!");
 
