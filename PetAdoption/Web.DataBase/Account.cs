da51a1a389aa5b521be036e6c275b17ff0049378
zhixinhe <zhixin.he@hotmail.com>
2022-04-29 08:30:40 -0700

    Zhixin h (#14)
    * solution setup
    
    * web.DataBase and Web.Api is completed
    
    * modified gitignore
    
    Co-authored-by: kellykeng <101091586+kellykeng@users.noreply.github.com>

---
+++ PetAdoption/Web.DataBase/Account.cs
@@ -0,0 +1,17 @@
+﻿namespace WebData.Models;
+public class Account
+{
+    public int AccountId { get; set; }
+    public string UserName { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Password { get; set; } = "123456";
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Zip { get; set; }
+    public string Country { get; set; }
+}
+
