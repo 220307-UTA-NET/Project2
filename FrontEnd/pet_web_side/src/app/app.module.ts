b5a789c70e084f56c9831feebfdada2784403cb6
cubidesrojas <cubideschristian8@gmail.com>
2022-05-02 14:57:24 -0500

    buttom angular

--- FrontEnd/pet_web_side/src/app/app.module.ts
+++ FrontEnd/pet_web_side/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from './app.component';
 import { PetImagesComponent } from './pet-images/pet-images.component';
 
@@ -10,7 +10,8 @@ import { PetImagesComponent } from './pet-images/pet-images.component';
     PetImagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- FrontEnd/pet_web_side/src/app/pet-images/pet-images.component.ts
+++ FrontEnd/pet_web_side/src/app/pet-images/pet-images.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-pet-images',
@@ -7,13 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PetImagesComponent implements OnInit {
   
-
+  postedFile!: File ;
   pet_name= "pluto";
-  constructor() { }
+  
+  constructor( private http: HttpClient) { }
 
   ngOnInit(): void {
   }
-  onFileSelected(event: any){ 
-    console.log(event);}
+  onFileSelected(event: any) 
+  { 
+    this.postedFile=<File>event.target.files[0]; 
+   
+  }
+  onUpload(){
+    const Form_Data = new FormData();
+    Form_Data.append('postedFile', this.postedFile,this.postedFile.name);
+    this.http.post('https://localhost:7042/api/pet_image_',Form_Data).subscribe(res =>{console.log(res)});
+  }
 
 }
