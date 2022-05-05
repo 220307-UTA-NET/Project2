164b0f16bc417b4af7ab44748c5d28502586b729
Kelly Keng <kelly.keng1@gmail.com>
2022-05-04 20:56:15 -0400

    fixing merge errors still

--- ZhixinAndChristianMerge/MyWebAPI/Models/PetWaitingList.cs
+++
@@ -1,9 +0,0 @@
-namespace Webpet.Models;
-
-public class PetWaitingList
-{
-    public int PetWaitingListId { get; set; }
-    public int PetId { get; set; }
-    public int WaitFor { get; set; } //1-adoption, 2-sitting
-    public int AccountId { get; set; }
-}
